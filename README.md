c42be352ed1efbbbffe138806d6e98f3272a2a47
Olivier <80892986+Choc-a-Block@users.noreply.github.com>
2024-04-25 15:36:49 +0000

    Major refactor + New features

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Code-Appendix-Generator
 
 A simple tool to generate a code appendix for a programming project. Run the script in the root directory of your project and it will generate a text file with a list of all files, their contents, and their directory structure.
+---
+
+## Usage
+
+```bash
+python3 generate_code_appendix.py [--whitelist or --blacklist] [--output] [--dir] [--no-overwrite]
+```
+
+### Appendix Files
+The `.apdx` file is a file that you can choose to add to your project. The purpose of this is to stipulate to the appendix generator either which files to include or which to exclude from the appendix. Each line in the file should be a regular expression that will be matched against the file path. If the file path matches any of the regular expressions in the `.apdx` file, the file will be included in the appendix.
+
+
+- `--whitelist`: Use the `.apdx` file to specify exactly which files to **include**. (Optional)
+- `--blacklist`: Use the `.apdx` file to specify exactly which files to **exclude**. (Optional)
+(The default behaviour is to use the .apdx file as an ignore list, similar to `.gitignore`)
+- `--output`: Output file path. Default is `output.txt`. (Optional)
+- `--dir`: Directory to process. De`ault is the current directory. (Optional)
+- `--no-overwrite`: Do not overwrite existing output file. (Optional)
+
+## Example usage:
+
+> I want to generate an appendix for my project in the directory `/path/to/project` and I want to include only the files that match the regular expressions in my `.apdx`. I want the output to be written to `my_appendix.txt`.
+```bash
+python3 generate_code_appendix.py --whitelist --output my_appendix.txt --dir /path/to/project
+```
+
--- appendix-generator.py
+++ appendix-generator.py
@@ -5,16 +5,19 @@ import re
 
 HEADER = '-' * 25 + 'Contents' + '-' * 25
 FOOTER = '-' * 24 + '/Contents' + "-"*25
-APDXIGNORE = '.apdxignore'
+APDX = '.apdx'
+CHUNK_SIZE = 1048576 # 1MB
 
 def load_apdxignore():
-    if os.path.exists(APDXIGNORE):
-        with open(APDXIGNORE, 'r') as f:
+    if os.path.exists(APDX):
+        with open(APDX, 'r') as f:
             return f.read().splitlines()
     return []
 
-def check_file(file_path, ignore_files):
-    return not any(re.search(ignore, file_path) for ignore in ignore_files)
+def check_file(file_path, ignore_files, blacklist):
+    if blacklist:
+        return not any(re.search(ignore, file_path) for ignore in ignore_files)
+    return any(re.search(ignore, file_path) for ignore in ignore_files)
 
 def log_error(file_path, e):
     logging.error(f"Error decoding file: {file_path}: {e}")
@@ -22,7 +25,7 @@ def log_error(file_path, e):
 def write_to_file(output_file, file, root, file_path):
     output_file.write(f"File: {file}\nDirectory: {root}\n{HEADER}\n")
     with open(file_path, 'r', encoding='utf-8') as f:
-        if os.path.getsize(file_path) > 1048576: # 1 MB
+        if os.path.getsize(file_path) > CHUNK_SIZE: 
             while True:
                 chunk = f.read(1024)
                 if not chunk:
@@ -32,32 +35,38 @@ def write_to_file(output_file, file, root, file_path):
             output_file.write(f.read())
     output_file.write(f"\n{FOOTER}\n\n")
 
-def process_files(directory, ignore_files, output_file):
+def process_files(directory, ignore_files, output_file, blacklist):
     for root, dirs, files in os.walk(directory):
         for file in files:
             try:
                 file_path = os.path.join(root, file)
-                if check_file(file_path, ignore_files):
+                if check_file(file_path, ignore_files, blacklist):
                     write_to_file(output_file, file, root, file_path)
             except (IOError, PermissionError) as e:
                 log_error(file_path, e)
             except UnicodeDecodeError as e:
                 logging.error(f"Error decoding file: {file_path}: {e}")
+            except UnicodeEncodeError as e:
+                logging.error(f"Error encoding file: {file_path}: {e}")
 
-def main(use_apdxignore=True, output_file_path="output.txt", directory=".", overwrite=True):
+def main(blacklist=True, output_file_path="output.txt", directory=".", overwrite=True):
     logging.basicConfig(level=logging.INFO)
-    logging.info(f"Parameters: use_apdxignore={use_apdxignore}, output_file_path={output_file_path}, directory={directory}, overwrite={overwrite}")
+    logging.info(f"Parameters: apdx_operating_mode={'Blacklist' if blacklist else 'Whitelist'}, output_file_path={output_file_path}, directory={directory}, overwrite={overwrite}")
     if overwrite and os.path.exists(output_file_path):
         os.remove(output_file_path)
-    ignore_files = load_apdxignore() if use_apdxignore else []
+    ignore_files = load_apdxignore() if os.path.exists(APDX) else []
     with open(output_file_path, 'a') as output_file:
-        process_files(directory, ignore_files, output_file)
+        process_files(directory, ignore_files, output_file, blacklist)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate an appendix.')
-    parser.add_argument('--no-ignore', dest='use_apdxignore', action='store_false', help='Do not use .apdxignore file')
+    parser.add_argument('--blacklist', dest='apdx_blacklist', action='store_true', help='Use .apdx as a blacklist')
+    parser.add_argument('--whitelist', dest='apdx_whitelist', action='store_true', help='Use .apdx as a whitelist')
     parser.add_argument('--output', dest='output_file_path', default='output.txt', help='Output file path')
     parser.add_argument('--dir', dest='directory', default='.', help='Directory to process')
     parser.add_argument('--no-overwrite', dest='overwrite', action='store_false', help='Do not overwrite existing output file')
     args = parser.parse_args()
-    main(args.use_apdxignore, args.output_file_path, args.directory, args.overwrite)
+    blacklist = args.apdx_blacklist
+    whitelist = args.apdx_whitelist
+    args.apdx_blacklist = blacklist or not whitelist
+    main(args.apdx_blacklist, args.output_file_path, args.directory, args.overwrite)
